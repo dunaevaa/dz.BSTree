080547607cf0a9f554e298b3c936d4921989fa14
dunaevaa <32384871+dunaevaa@users.noreply.github.com>
2018-06-13 16:53:45 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include<iostream>
 #include<locale>
+#include<string>
+#include<fstream>
 using namespace std;
 
 struct Node {
     int data;
     Node *left;
     Node *right;
+    Node *parent;
 };
 
 class BSTree {
@@ -13,17 +16,17 @@ class BSTree {
         Node *root;
     public:
         BSTree() { root = nullptr; }
-        auto insert(Node*& root, int value) -> Node* {
+    
+        auto insert(Node*& root, Node*& parent, Node*& value) -> Node* {
             if (root == nullptr) {
-                root = new Node { value, nullptr, nullptr };
-            } else {
-                if (root->data < value) 
-                    root->left = insert(root->left, value);
-                else if (root->data > value) 
-                    root->right = insert(root->right, value);
-                else if (root -> data == value) {
-                    cout << "дерево имеет этот узел" << endl;
-                    return root;
+		        root = value;
+		        if(root != this->root)
+                    root->parent = parent;
+	        } else {
+		        if (root->data < value->data) {
+			        root->left = insert(root->left, root, value);
+		        } else if (root->data > value->data) {
+                    root->right = insert(root->right, root, value);
                 }
             }
             return root;
@@ -38,6 +41,7 @@ class BSTree {
             }
         }
         ~BSTree() {
+            cout << "Был вызван деструктор" << endl;
             delete_tree(root);
         }
         void show(Node*node, int stage) {
@@ -49,10 +53,12 @@ class BSTree {
             }
             cout << "(" << node->data << ")" << endl;
             if (node->right != nullptr)
-                show(node->right, stage + 1);
+                show(node->right, stage+1);
         }
         void show() {
-            show (root, 1);
+            if (root != nullptr) 
+                show (root, 1);
+            else cout << "Дерево пустое" << endl;
         }
         void direct_bypass(Node* root) {
             if (root != nullptr) {
@@ -76,13 +82,158 @@ class BSTree {
                 cout << root->data << " ";
             }
         }
-        void select_bypass( char choice_bypass ) {
+        void select_bypass(char choice_bypass) {
             if (choice_bypass == 'a') 
                 direct_bypass(root);
             if (choice_bypass == 'b') 
                 cross_bypass(root);
             if (choice_bypass == 'c') 
                 reverse_bypass(root);
+        }
+        
+        bool upload_to_file(string& road)  {
+            ofstream fout(road.c_str());
+            if(!fout.is_open() || root == nullptr) {
+                cout << "Файл не открыт или дерево пустое" << endl;
+                return false;
+            }
+            direct_bypass(root, fout);
+            return true;
+        }
+        void direct_bypass(Node* root, ofstream &fout) {
+            if (root != nullptr) {
+                fout << root->data << " ";
+                direct_bypass(root->right, fout);
+                direct_bypass(root->left, fout);
+            }
+        }
+
+        bool unload_from_file(string& road) {
+            ifstream fin(road.c_str());
+            if (!fin.is_open()) {
+                cout << "файл не открыт";
+                return false;
+            }
+            if (root) 
+                delete_tree(root);
+            int value;
+            while(fin >> value) 
+                insert(value);
+            return true;
+        }
+
+        bool exists(int value) const {
+            if(!root) {
+                cout << "дерево пустое" << endl;
+                return false;
+            } else if (root) {
+                Node *current = root;
+                while(current != nullptr) {
+                    if(value < current->data) 
+                        current = current->right;
+                    else if(value > current->data) 
+                        current = current->left;
+                    else if(current->data == value) 
+                    break;
+                }
+                if(current == nullptr) {
+                    cout << "узел не найден" << endl;
+                    return false;
+                } else {
+                    cout << "узел найден" << endl;
+                    return true;
+                }
+            }
+        }
+    
+        auto delete_node(Node *&root, int value) -> bool {
+            if(!root) {
+		        cout << "дерево пустое" << endl;
+		        return false;
+	        } else if(root) {
+		        Node *current = root;
+		        while (current != nullptr) {
+			        if (value < current->data) 
+				        current = current->right;
+			        else if (value > current->data) 
+				        current = current->left;
+			        else if (current->data == value)
+				        break;
+                }
+                if (current == nullptr) {
+                    cout << "узел не найден" << endl;
+                    return false;
+                }
+                Node* left = nullptr;
+                Node* right = nullptr;
+		        if (current == root) {
+		            if (current->right!=nullptr && current->left!=nullptr) {
+		                root=current->right;
+		                insert(root,root,current->left);
+		            } else if (current->right!=nullptr && current->left==nullptr) 
+			            root = current->right;
+		              else if (current->left!=nullptr && current->right==nullptr) 
+		                root = current->left;
+                      else if (current->left==nullptr && current->right==nullptr)
+		                root=nullptr;
+			        delete current;
+			        cout << "успешно удалено" << endl;
+			        return true;
+		        } else {
+                    if (current->right != nullptr && current->left == nullptr) {
+				        if (current == current->parent->left)
+					        current->parent->left = nullptr;
+				        else
+					        current->parent->right = nullptr;
+				        right = current->right;
+				        insert(root, root, right);
+			        } else if (current->left != nullptr && current->right == nullptr) {
+				        if (current == current->parent->left)
+					        current->parent->left = nullptr;
+				        else
+					        current->parent->right = nullptr;
+				        left = current->left;
+				        insert(root, root, left);
+			        } else if (current->right != nullptr && current->left != nullptr) {
+				        left = current->left;
+				        right = current->right;
+				        if (current == current->parent->left)
+					        current->parent->left = nullptr;
+				        else
+					        current->parent->right = nullptr;
+				        insert(root, root, left);
+				        insert(root, root, right);
+			        } else {
+				        if (current == current->parent->left)
+					        current->parent->left = nullptr;
+				        if (current == current->parent->right)
+					        current->parent->right = nullptr;
+			        }
+			        delete current;
+			        cout << "Удалено успешно" << endl;
+			        return true;
+		        }
+	        }
+        }
+        auto insert(Node*& root, Node*& parent, int value) -> Node* {
+	        if (root == nullptr) {
+		        root = new Node {value, nullptr, nullptr, nullptr};
+		        if (root != this->root)
+			        root->parent = parent;
+	        } else {
+		        if (root->data < value) 
+			        root->left = insert(root->left, root, value);
+		        else if (root->data > value) 
+			        root->right = insert(root->right, root, value);
+		        else if (root -> data == value) {
+			        cout << "Дерево имеет этот узел" << endl;
+			        return root;
+		        }
+	        }
+	        return root;
+        }
+        auto delete_node(int value) -> void {
+            delete_node(root,value);
         }
 };
 
@@ -108,6 +259,8 @@ int main(int argc, char* argv[]) {
     char choice_bypass;
     int choice = 0;
     while (true) {
+        string road;
+        int value = 0;
         menu();
         cin >> choice;
         switch (choice) {
@@ -118,15 +271,20 @@ int main(int argc, char* argv[]) {
                     tree.select_bypass(choice_bypass);
                     cout << endl;
             break;
-            case 3:
+            case 3: cin >> value;
+                    tree.insert(value);
             break;
-            case 4:
+            case 4: cin >> value;
+                    tree.delete_node(value);
             break;
-            case 5:
+            case 5: cin >> road;
+                    tree.upload_to_file(road);
             break;
-            case 6:
+            case 6: cin >> road;
+                    tree.unload_from_file(road);
             break;
-            case 7:
+            case 7: cin >> value;
+                    tree.exists(value);
             break;
             case 8: cout << "Вы уверены, что хотите выйти из программы?";
                     cin >> choice_exit;
