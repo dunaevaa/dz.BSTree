bf51bc65281e92d3ad96e1b3e92f249bee34a164
dunaevaa <32384871+dunaevaa@users.noreply.github.com>
2018-06-14 10:29:31 +0300

    Update and rename bstree.hpp to BSTREE.hpp

--- include/BSTREE.hpp
+++ include/BSTREE.hpp
@@ -1,13 +1,15 @@
-namespace BSTRee {
-struct Node {
+#include<iostream>
+#include<fstream>
+namespace BSTREE{
+struct Node{
     int data;
     Node *left;
     Node *right;
     Node *parent;
 };
 
-class BSTree {
-  private:
+class BSTree{
+    private:
     Node *root;
     auto insert(Node*& root,Node*& parent,Node*& value)-> Node*;
     void delete_tree(Node *&data);
@@ -15,19 +17,20 @@ class BSTree {
     void direct_bypass(Node* root);
     void cross_bypass(Node* root);
     void reverse_bypass(Node* root);
-    void direct_bypass(Node* root, ofstream &fout);
+    void direct_bypass(Node* root, std::ofstream &fout);
     auto delete_node(Node *&root, int value) ->bool;
     auto insert(Node*& root, Node*& parent, int value) -> Node*;
-    
-  public:
+
+
+    public:
     BSTree();
     void insert(int value);
     void show();
     void select_bypass( char choice_bypass );
-    bool upload_to_file(string& road);
-    bool unload_from_file(string& road);
+    bool upload_to_file(std::string& road);
+    bool unload_from_file(std::string& road);
     bool exists(int value) const;
     auto delete_node(int value) ->void;
     ~BSTree();
-  };
-  }
+    };
+}
